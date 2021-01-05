3e72aa0dd2bdbdff863e43f2bdcc7dcad2558092
anicodesit <ani.kulka@gmail.com>
2021-01-04 21:36:47 -0500

    Add project files.

---
+++ Black Jack Repo/BlackJackGameRepo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackGameRepo
+
+{
+    public class GameRepo
+    {
+        public List<int> DealerHand = new List<int>();
+
+        public List<int> PlayerHand = new List<int>();
+
+        //add the deck later, use random number assingment for now
+
+               
+        Random random = new Random(DateTime.Now.Millisecond);
+        public int DealACard()
+        {
+            int thisRandomNumber = random.Next(1, 14);
+            return thisRandomNumber;
+        }
+
+        public void AssignToDealerHand(int card)
+        {
+            DealerHand.Add(card);
+        }
+        public void AssignToPlayerHand(int card)
+        {
+            PlayerHand.Add(card);
+        }
+        public List<int> DisplayDealerHand()
+        {
+            return DealerHand;
+        }
+        public List<int> DisplayPlayerHand()
+        {
+            return PlayerHand;
+        }
+        public void FinishDealersHand()
+        {
+            int count = 0;
+            foreach (int card in DealerHand)
+            {
+                count += card;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ BlackJackGame/BlackJackGameUI.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlackJackGameRepo;
+
+namespace BlackJackGame
+{
+    class BlackJackGameUI
+    {       
+        GameRepo repoWindow = new GameRepo();
+            public void RunMenu()
+            {           
+            int dealtCard = repoWindow.DealACard();           
+            repoWindow.AssignToDealerHand(dealtCard);
+            dealtCard = repoWindow.DealACard();
+            repoWindow.AssignToPlayerHand(dealtCard);
+            dealtCard = repoWindow.DealACard();
+            repoWindow.AssignToDealerHand(dealtCard);
+            dealtCard = repoWindow.DealACard();
+            repoWindow.AssignToPlayerHand(dealtCard);
+
+            List<int> dealer = repoWindow.DisplayDealerHand();
+            List<int> player = repoWindow.DisplayPlayerHand();
+            bool firstTime = true;
+            foreach   (int card in dealer)
+            {
+                if (firstTime == true)
+                {
+                    Console.WriteLine("X");
+                }
+                else
+                {
+                    Console.WriteLine(card);
+                }
+               
+            }
+            foreach (int card in player)
+            {
+                Console.WriteLine(card);
+            }
+            
+            
+            
+
+                bool isRunning = true;
+                while (isRunning)
+                {
+                    Console.WriteLine("Welcome to Black Jack!\n" +
+                        "1. \n" +
+                        "2. Create a new menu item\n" +
+                        "3. Delete an item from the menu\n" +
+                        "20. Closes the program");
+
+                    var userInput = Console.ReadLine();
+
+                    switch (userInput)
+                    {
+                        case "1":
+                            getAllMenuItems();
+                            break;
+                        case "2":
+                            CreateAMenuItem();
+                            break;
+                        case "3":
+                            RemoveAMenuItem();
+                            break;
+                        case "20":
+                            isRunning = false;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    Console.WriteLine("Hit 'Enter' to continue ");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+}
